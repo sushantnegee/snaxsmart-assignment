037f26bb5272d13e7ef00d4f1302dfdc7b7c5e25
sushantnegee <sushantnegee@gmail.com>
2023-04-06 23:35:17 +0530

    admins schema created

---
+++ server/models/adminModels.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const adminSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+},{
+    timestamps:true
+})
+
+const Admin = mongoose.model('Admin',adminSchema)
+
+module.exports = Admin;
